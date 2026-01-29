edf45b39820f31e87bd6cc8cf1fb132ef4e4cffd
Eunjin Kim <jennyeunjin@khu.ac.kr>
2026-01-29 12:54:59 +0900

    [Init] springboot 프로젝트 생성

---
+++ src/main/java/com/koscom/team5/ipo_check/IpoCheckApplication.java
@@ -0,0 +1,13 @@
+package com.koscom.team5.ipo_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpoCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpoCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koscom/team5/ipo_check/IpoCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koscom.team5.ipo_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpoCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
