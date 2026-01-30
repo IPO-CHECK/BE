391b0e268851dc5b78c9a35eb14f3063aa3cd379
HanGang <rhgksrkd@naver.com>
2026-01-30 23:30:57 +0900

    Test

--- src/main/java/com/koscom/team5/ipo_check/IpoCheckApplication.java
+++ src/main/java/com/koscom/team5/ipo_check/IpoCheckApplication.java
@@ -2,7 +2,9 @@ package com.koscom.team5.ipo_check;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+//@EnableScheduling
 @SpringBootApplication
 public class IpoCheckApplication {
 
