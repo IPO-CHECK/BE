e046380c1f4cbdf82210f0f394934b4da7688adf
HanGang <rhgksrkd@naver.com>
2026-02-02 04:20:00 +0900

    Feat: 사업 내용 추출

--- src/main/java/com/koscom/team5/ipo_check/IpoCheckApplication.java
+++ src/main/java/com/koscom/team5/ipo_check/IpoCheckApplication.java
@@ -2,11 +2,14 @@ package com.koscom.team5.ipo_check;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@EnableScheduling
 @SpringBootApplication
 public class IpoCheckApplication {
+@EnableConfigurationProperties(DartProperties.class)
+public class DartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IpoCheckApplication.class, args);
