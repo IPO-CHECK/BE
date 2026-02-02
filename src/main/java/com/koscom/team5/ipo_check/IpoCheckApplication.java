1e3946f0a3a818d858f1203ff01631bc45bdba08
HanGang <rhgksrkd@naver.com>
2026-02-02 09:45:39 +0900

    Feat: 상장된 기업들의 산업, 주요 제품 vector화

--- src/main/java/com/koscom/team5/ipo_check/IpoCheckApplication.java
+++ src/main/java/com/koscom/team5/ipo_check/IpoCheckApplication.java
@@ -1,7 +1,13 @@
 package com.koscom.team5.ipo_check;
 
+import financial.dart.config.DartProperties;
+import financial.dart.vector.properties.OpenAiProperties;
+import financial.dart.vector.properties.QdrantProperties;
+package com.koscom.team5.ipo_check;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -9,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class IpoCheckApplication {
 @EnableConfigurationProperties(DartProperties.class)
+@ConfigurationPropertiesScan(basePackageClasses = {QdrantProperties.class, OpenAiProperties.class})
 public class DartApplication {
 
 	public static void main(String[] args) {
